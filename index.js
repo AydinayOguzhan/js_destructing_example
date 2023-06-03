9ea1a98d6805552862c45c46d7fcfcf842ee72b7
Oğuzhan Aydınay <aydinayoguzhan@gmail.com>
2023-06-03 20:26:22 +0300

    First commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const values = [{id:1, name:"deneme", value:"örnek"},
+{id:2, name:"deneme2", value:"örnek2"},
+{id:3, name:"deneme3", value:"örnek3"},];
+
+
+
+const [...deger] = values;
+console.log(deger);
+
+// const [deneme] = values;
+// console.log(deneme);
+// const deneme = {}
+
+// values.forEach(element => {
+//     deneme.id = element.id,
+//     deneme.name = element.name,
+//     deneme.value = element.value
+// });
+// console.log(deneme);
